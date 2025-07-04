80a3630aeb75e191f967673910cf4f3237797b06
Javier Osuna <javiosuna01@gmail.com>
2025-07-04 18:08:28 +0200

    Testing the configuration and docker

--- src/main.py
+++ src/main.py
@@ -3,9 +3,22 @@ import mlflow
 
 def main():
     print("¡El entorno está funcionando correctamente!")
+
+    # Iniciar Spark
     spark = SparkSession.builder.appName("Test").getOrCreate()
     print("SparkSession creada con éxito")
-    print("MLflow versión:", mlflow.__version__)
+
+    # Crear un DataFrame de prueba
+    df = spark.createDataFrame([(1, "a"), (2, "b")], ["id", "valor"])
+    df.show()
+
+    # Probar MLflow
+    with mlflow.start_run(run_name="prueba_spark_mlflow") as run:
+        mlflow.log_param("param_prueba", 123)
+        mlflow.log_metric("metric_prueba", 0.99)
+        print("MLflow run ID:", run.info.run_id)
+
+    spark.stop()
 
 if __name__ == "__main__":
     main()
